61b54d690781c6813576c29540b220776a67bfe0
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-11-19 08:19:27 +0800

    feat: add remote MCP

--- cmd/90_mcp/agent.go
+++ cmd/90_mcp/agent.go
@@ -3,22 +3,36 @@ package main
 import (
 	"agenttry/mdl"
 	"agenttry/runner"
+	"context"
 	"log"
+	"os"
 	"os/exec"
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
+	"golang.org/x/oauth2"
 	"google.golang.org/adk/agent/llmagent"
 	"google.golang.org/adk/tool"
 	"google.golang.org/adk/tool/mcptoolset"
 )
 
 func main() {
-	cfg := mcptoolset.Config{
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_PAT")})
+
+	githubMCPServer, err := mcptoolset.New(mcptoolset.Config{
+		Transport: &mcp.StreamableClientTransport{
+			Endpoint:   "https://api.githubcopilot.com/mcp/",
+			HTTPClient: oauth2.NewClient(context.Background(), ts),
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = githubMCPServer
+
+	mcpImageServer, err := mcptoolset.New(mcptoolset.Config{
 		Transport:  &mcp.CommandTransport{Command: exec.Command("npx", "-y", "@modelcontextprotocol/server-everything")},
 		ToolFilter: tool.StringPredicate([]string{"getTinyImage", "echo"}),
-	}
-
-	mcpImageServer, err := mcptoolset.New(cfg)
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,8 +43,7 @@ func main() {
 		Instruction: `Use the MCP tool from the toolset to generate images or to parrot/echo  user queries
 		when calling the getTinyImage tool issue the 'includeImage' argument.
 		The getTinyImage tool produces base-64 encoded data. When asked to generate images, output the raw data verbatim and state that is base-64 encoded.`,
-		//Instruction: `You are a question answering agent`,
-		Toolsets: []tool.Toolset{mcpImageServer},
+		Toolsets: []tool.Toolset{mcpImageServer, githubMCPServer}, // To list tools: use a prompt like: "What tools do you have?"
 	})
 	if err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -36,7 +36,7 @@ require (
 	go.opentelemetry.io/otel/trace v1.38.0 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
 	golang.org/x/net v0.46.0 // indirect
-	golang.org/x/oauth2 v0.32.0 // indirect
+	golang.org/x/oauth2 v0.33.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
