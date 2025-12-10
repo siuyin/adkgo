f598da43006905d963258cff9d7c47f215909cce
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-12-10 15:06:54 +0800

    feat: update mdl for vertexai config

--- mdl/mdl.go
+++ mdl/mdl.go
@@ -12,6 +12,19 @@ import (
 
 // FromEnv defines an llm model from environment variable MODEL
 func FromEnv() model.LLM {
+	if os.Getenv("GOOGLE_API_KEY") == "" {
+		log.Printf("model: %q, project: %q", os.Getenv("MODEL"), os.Getenv("GOOGLE_CLOUD_PROJECT"))
+		model, err := gemini.NewModel(context.Background(),
+			os.Getenv("MODEL"),
+			&genai.ClientConfig{}, // values set from environment
+		)
+		if err != nil {
+			log.Fatalf("could not create model on Vertex AI: %v", err)
+		}
+
+		return model
+	}
+
 	model, err := gemini.NewModel(context.Background(),
 		os.Getenv("MODEL"),
 		&genai.ClientConfig{APIKey: os.Getenv("GOOGLE_API_KEY")})
@@ -20,4 +33,5 @@ func FromEnv() model.LLM {
 	}
 
 	return model
+
 }
